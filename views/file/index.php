52840c3088202c278959031de8e62fb979f5f09b
Dmitrii N <ndmitry.web@gmail.com>
2020-11-13 22:27:26 +0400

    CRUD File

---
+++ views/file/index.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+use yii\widgets\Pjax;
+/* @var $this yii\web\View */
+/* @var $searchModel app\models\FileSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = 'Files';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="file-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Create File', ['create'], ['class' => 'btn btn-success']) ?>
+    </p>
+
+    <?php Pjax::begin(); ?>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'id',
+            'name',
+            'mime',
+            'size',
+            'status',
+
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+
+    <?php Pjax::end(); ?>
+
+</div>
