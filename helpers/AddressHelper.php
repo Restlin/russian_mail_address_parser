541efb25052e3df7d3c81d8e8208bc9c9e541a98
pky <pky@pkyDeb>
2020-11-14 17:37:35 +0400

    fix

--- helpers/AddressHelper.php
+++ helpers/AddressHelper.php
@@ -8,6 +8,7 @@ namespace app\helpers;
  * @author restlin
  */
 class AddressHelper {
+
     /**
      * Выделение адреса в строке сырых данных по словарику адресных частей
      * @param string $content сырой контент
@@ -15,18 +16,21 @@ class AddressHelper {
      * @return string
      */
     public static function findAddress(string $content, array $addressParts): string {
-        foreach([";", "\t", ","] as $delimiter) {
+        foreach ([";", "\t", ","] as $delimiter) {
+            $content = preg_replace('/\d{1,2}.\d{1,2}.\d{2,4}(\s\d{1,2}:\d{1,2}(:\d{1,2}?)?)/', '', $content);
+            $content = preg_replace('/\d+,\d+E\+\d+|true|false|\s+0(\s?)/i', '', $content);
+            $content = preg_replace('/\s{2,}/i', ' ', $content);
             $columns = explode($delimiter, mb_strtolower($content, 'UTF-8'));
             $count = count($columns);
-            if($count > 1 && $count < 100) {
+            if ($count > 1 && $count < 100) {
                 break;
             }
         }
         $result = [];
-        foreach($columns as $column) {
+        foreach ($columns as $column) {
             $elements = preg_split('\/s\ui', $column);
-            foreach($elements as $element) {
-                if(in_array($element, $addressParts)) {
+            foreach ($elements as $element) {
+                if (in_array($element, $addressParts)) {
                     $result[] = $column;
                     break;
                 }
@@ -34,4 +38,5 @@ class AddressHelper {
         }
         return implode(' ', $result);
     }
+
 }
