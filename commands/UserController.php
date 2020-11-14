d679b39b5faba76fe47074e7c0718f195b6785a1
pky <pky@pkyDeb>
2020-11-14 15:19:56 +0400

    отправка адреса на проверку

--- commands/UserController.php
+++ commands/UserController.php
@@ -39,45 +39,4 @@ class UserController extends Controller {
         $user->save();
     }
 
-    public function actionTest() {
-        $rows = Row::find()->where(['is not', 'address_base', null])->all();
-        foreach ($rows as $row) {
-            /* @var $service PostApiService */
-            $service = Yii::$container->get(PostApiService::class);
-            $data = $service->checkAddress($row);
-            if ($this->cheskData($data)) {
-                $row->address_new = $data['addr']['outaddr'];
-                $row->status = Row::STATUS_DONE;
-            } else {
-                $row->status = Row::STATUS_ERROR;
-            }
-            $row->save();
-        }
-    }
-
-    protected function cheskData(array $data): bool {
-        $result = false;
-        $state = null;
-        if (key_exists('state', $data)) {
-            $state = $data['state'];
-        }
-        $missing = null;
-        $accuracy = null;
-        if (key_exists('addr', $data)) {
-            if (key_exists('missing', $data['addr'])) {
-                $missing = ($data['addr']['missing']);
-            }
-            if (key_exists('accuracy', $data['addr'])) {
-                $accuracy = ($data['addr']['accuracy']);
-            }
-        }
-
-        if (in_array($state, ['301', '302']) && $accuracy && $accuracy < 300 && ($missing === null || in_array($missing, ['F', 'NF']))) {
-            $result = true;
-        } elseif (($missing === null && $accuracy && $accuracy > 300) || ($accuracy && in_array($missing, ['SNF', 'TSNF']) )) {
-            $result = false;
-        }
-        return $result;
-    }
-
 }
